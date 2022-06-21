e1bd56719fa2cad53d497d300363ea0407fddade
James Adakole <j.adakole@taxaide.com.ng>
2022-06-21 23:04:21 +0100

    contract deployed and dapp init

---
+++ hardhat-project/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = '0x69f4831b7A427079d7b1Db6D5a1C007ce7f893A9';
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
