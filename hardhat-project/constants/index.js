39fa2065719aae6573988303790bf100014dd6d8
James Adakole <j.adakole@taxaide.com.ng>
2022-06-21 23:47:33 +0100

    new contract made and metadata url changed to vercel url

--- hardhat-project/constants/index.js
+++ hardhat-project/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = '0x69f4831b7A427079d7b1Db6D5a1C007ce7f893A9';
+const WHITELIST_CONTRACT_ADDRESS = '0xc7D353F77a49C80135Bc1B552c0e1130889E168b';
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'https://nft-collection-one-tau.vercel.app/api/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
